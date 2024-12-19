3afa0bb5ed3e77fd1f3b4f619a918ca48dae7cc3
Lanthong <98867063+lxcs20@users.noreply.github.com>
2024-12-19 23:53:44 +0700

    .

--- mqtt-publisher.js
+++ mqtt-publisher.js
@@ -1,9 +1,6 @@
 import mqtt from "mqtt";
 
 const brokerUrl = "mqtt://127.0.0.1:1883";
-// const brokerUrl = "6a60f28984b7463ea25d6758548c25b3.s1.eu.hivemq.cloud:8883";
-
-// Connect to RabbitMQ
 const client = mqtt.connect(brokerUrl, {
     username: "guest",
     password: "guest",
